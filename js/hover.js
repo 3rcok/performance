56586e12023da24d7b20059620a59f02652a5f2c
Cornelia <cornelia_schulz@hotmail.com>
2014-11-19 21:20:35 +1300

    minified js and css files

--- js/hover.js
+++ js/hover.js
@@ -1,18 +1,1 @@
-/**
- * Created by cornelia on 19/11/14.
- */
-$(document).ready(function() {
-    var portfolio = $('.portfolio');
-   portfolio.mouseenter(function () {
-       $(this).children('.portfolio-image').animate( {
-           opacity: 0.25
-           }, 100);
-       $(this).children('.portfolio-link').css('color', 'gray');
-   });
-    portfolio.mouseleave(function () {
-        $(this).children('.portfolio-image').animate( {
-            opacity: 1
-        }, 100);
-        $(this).children('.portfolio-link').css('color', '#fff');
-    })
-});
+$(document).ready(function(){var e=$(".portfolio");e.mouseenter(function(){$(this).children(".portfolio-image").animate({opacity:.25},100);$(this).children(".portfolio-link").css("color","gray")});e.mouseleave(function(){$(this).children(".portfolio-image").animate({opacity:1},100);$(this).children(".portfolio-link").css("color","#fff")})})
